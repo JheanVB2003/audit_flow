b0b86066755952e3a31a8d7f84358766752a960c
Jhean_Victo <jheanbarbosa1233@gmail.com>
2026-01-08 16:55:12 -0300

    feat: document ingestion pipeline.

---
+++ src/main/java/com/auditflow/backend/repository/DocumentRepository.java
@@ -0,0 +1,9 @@
+package com.auditflow.backend.repository;
+
+import com.auditflow.backend.entity.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+}
